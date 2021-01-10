debfc972c1d5c60b0bfb16e0e9651c144831516c
fernando <ferpb1999@gmail.com>
2021-01-10 21:00:49 +0100

    Añadir ILP en práctica 2

--- Practica2/vertex_cover.py
+++ Practica2/vertex_cover.py
@@ -23,11 +23,13 @@ def leer_entrada(ficheroDeEntrada):
         return [aristas, vertices]
 
 
+# Programación lineal
+
 def MinWeightVC(grafo):
     solver = pywraplp.Solver(
         'SolveSimpleSystem', pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
 
-    variables = [[]] * len(grafo[1])
+    variables = [None] * len(grafo[1])
     objective = solver.Objective()
 
     # Definicion de la funcion objetivo
@@ -39,14 +41,16 @@ def MinWeightVC(grafo):
         objective.SetCoefficient(variables[i], grafo[1][i])
     objective.SetMinimization()
 
-    # Definicio de la restriciones
+    # Definicion de la restriciones
 
     # Restriciones de las aristas
+    # x(u) + x(v) >= 1
     for i in range(0, len(grafo[0])):
         arista = grafo[0][i]
         solver.Add(variables[arista[0]] + variables[arista[1]] >= 1.0)
 
     # Restriciones de los vertices
+    # 0 <= x(v) <= 1
     for i in range(0, len(grafo[1])):
         solver.Add(0.0 <= variables[i] <= 1.0)
 
@@ -73,6 +77,81 @@ def MinWeightVC(grafo):
             print('The solver could not solve the problem.')
 
 
+def wVC_entero(grafo):
+    # print("\nEntero")
+    # print("======")
+    # print(grafo)
+
+    # Crear solver MIP utilizando SCIP como backend
+    # solver = pywraplp.Solver(
+    #    'SolveSimpleSystem', pywraplp.Solver.SCIP_MIXED_INTEGER_PROGRAMMING)
+    solver = pywraplp.Solver.CreateSolver('SCIP')
+
+    variables = []
+    objective = solver.Objective()
+
+    # Definicion de la funcion objetivo
+    # min (sum(w_v * x_v)) donde v es un vertice del grafo.
+    # Por cada vertice se crea una variable x_v
+
+    for i in range(0, len(grafo[1])):
+        variables.append(solver.IntVar(0, solver.infinity(), str(i)))
+        # print(variables[i])
+
+        # print("Coeficiente", variables[i], grafo[1][i])
+
+        objective.SetCoefficient(variables[i], grafo[1][i])
+
+    objective.SetMinimization()
+
+    # print('Number of variables', solver.NumVariables())
+
+    # Definicion de la restriciones
+
+    # Restriciones de las aristas
+    # x(u) + x(v) >= 1
+    for i in range(0, len(grafo[0])):
+        arista = grafo[0][i]
+        solver.Add(variables[arista[0]] + variables[arista[1]] >= 1.0)
+        # print("Restriccion", variables[arista[0]], "+", variables[arista[1]])
+        # print("Restriccion", arista[0], "+", arista[1])
+
+    # Restriciones de los vertices
+    # x(v) = {0, 1}
+    for i in range(0, len(grafo[1])):
+        solver.Add(0 <= variables[i] <= 1)
+
+    # print('Number of constraints', solver.NumConstraints())
+
+    status = solver.Solve()
+    solucion = []
+
+    if status == solver.OPTIMAL:
+
+        solucion = []
+
+        # print('Solution:')
+        # print('Objective value = ', solver.Objective().Value())
+        for i in range(0, len(grafo[1])):
+            v = variables[i]
+            # print(str(v), "=", v.solution_value())
+            if variables[i].solution_value() >= 1:
+                solucion.append(i)
+
+        print(len(grafo[1]), ' ', len(grafo[0]),
+              ' ', solver.Objective().Value())
+        print(solucion)
+
+    else:
+
+        if status == solver.FEASIBLE:
+            print('A potentially suboptimal solution was found.')
+        else:
+            print('The solver could not solve the problem.')
+
+
+# Pricing method
+
 def acum_vertice(pesos_aristas, vertice, grafo):
 
     acum = 0.0
@@ -95,7 +174,7 @@ def is_tight(grafo, vertice, pesos_aristas):
     return w_i == acum
 
 
-def incremetar_peso(pesos_aristas, num_arista, arista, grafo):
+def incrementar_peso(pesos_aristas, num_arista, arista, grafo):
 
     resto_v1 = resto_vertice(pesos_aristas, arista[0], grafo)
     resto_v2 = resto_vertice(pesos_aristas, arista[1], grafo)
@@ -114,7 +193,7 @@ def PricingMethod(grafo):
     for e in range(0, len(grafo[0])):
         arista = grafo[0][e]
         if((not is_tight(grafo, arista[0], pesos_aristas)) and (not is_tight(grafo, arista[1], pesos_aristas))):
-            incremetar_peso(pesos_aristas, e, arista, grafo)
+            incrementar_peso(pesos_aristas, e, arista, grafo)
 
     for v in range(0, len(grafo[1])):
         if(is_tight(grafo, v, pesos_aristas)):
@@ -147,7 +226,7 @@ def PricingMethod_bueno(grafo):
 
     while arista != []:
 
-        incremetar_peso(pesos_aristas, e, arista, grafo)
+        incrementar_peso(pesos_aristas, e, arista, grafo)
         arista,  e = get_arista(grafo, pesos_aristas)
 
     for v in range(0, len(grafo[1])):
@@ -162,5 +241,13 @@ def PricingMethod_bueno(grafo):
 # grafo[0] representa el conjunto de aristas
 # grafo[1] representa el conjunto de vertices
 grafo = leer_entrada(sys.argv[1])
+print("Grafo:", grafo)
+
+print("LP entero")
+wVC_entero(grafo)
+
+print("LP relajado")
 MinWeightVC(grafo)
+
+print("Pricing method")
 PricingMethod_bueno(grafo)
